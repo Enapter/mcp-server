9cf1507d920fe0963a182ef269dbe78e6179ae87
Roman Novatorov <roman.novatorov@gmail.com>
2026-01-20 11:09:19 +0100

    mcp: add models

---
+++ src/enapter_mcp_server/mcp/models.py
@@ -0,0 +1,55 @@
+import enum
+from typing import Self
+
+import enapter
+import pydantic
+
+
+class Site(pydantic.BaseModel):
+
+    id: str
+    name: str
+    timezone: str
+
+    @classmethod
+    def from_domain(cls, site: enapter.http.api.sites.Site) -> Self:
+        return cls(id=site.id, name=site.name, timezone=site.timezone)
+
+
+class DeviceType(enum.StrEnum):
+
+    LUA = "LUA"
+    VIRTUAL_UCM = "VIRTUAL_UCM"
+    HARDWARE_UCM = "HARDWARE_UCM"
+    STANDALONE = "STANDALONE"
+    GATEWAY = "GATEWAY"
+    LINK_MASTER_UCM = "LINK_MASTER_UCM"
+    LINK_SLAVE_UCM = "LINK_SLAVE_UCM"
+    EMBEDDED_UCM = "EMBEDDED_UCM"
+    NATIVE = "NATIVE"
+
+
+class DeviceConnectivityStatus(enum.StrEnum):
+
+    UNKNOWN = "UNKNOWN"
+    ONLINE = "ONLINE"
+    OFFLINE = "OFFLINE"
+
+
+class Device(pydantic.BaseModel):
+
+    id: str
+    blueprint_id: str
+    name: str
+    site_id: str
+    type: DeviceType
+
+    @classmethod
+    def from_domain(cls, device: enapter.http.api.devices.Device) -> Self:
+        return cls(
+            id=device.id,
+            blueprint_id=device.blueprint_id,
+            name=device.name,
+            site_id=device.site_id,
+            type=DeviceType(device.type.value),
+        )
