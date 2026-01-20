34655f24e3b56403e6aabf8405dd79afe1412ec3
Roman Novatorov <roman.novatorov@gmail.com>
2026-01-20 15:48:17 +0100

    chore(setup): pin enapter sdk version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def main() -> None:
         author="Roman Novatorov",
         author_email="rnovatorov@enapter.com",
         install_requires=[
-            "enapter==0.14.*",
+            "enapter==0.14.3",
             "fastmcp==2.14.*",
         ],
     )
