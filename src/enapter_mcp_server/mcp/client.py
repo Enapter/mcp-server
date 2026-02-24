d3686c3142f9a8916af193e3246f16542ee28ad5
Roman Novatorov <roman.novatorov@gmail.com>
2026-02-24 09:49:44 +0100

    feat(mcp): client: proxy x-enapter-auth-user

--- src/enapter_mcp_server/mcp/client.py
+++ src/enapter_mcp_server/mcp/client.py
@@ -5,13 +5,22 @@ import fastmcp
 
 class Client:
 
-    def __init__(self, url: str, enapter_auth_token: str | None = None) -> None:
-        self._client = self._new_client(url, enapter_auth_token)
+    def __init__(
+        self,
+        url: str,
+        enapter_auth_token: str | None = None,
+        enapter_auth_user: str | None = None,
+    ) -> None:
+        self._client = self._new_client(url, enapter_auth_token, enapter_auth_user)
 
-    def _new_client(self, url: str, enapter_auth_token: str | None) -> fastmcp.Client:
+    def _new_client(
+        self, url: str, enapter_auth_token: str | None, enapter_auth_user: str | None
+    ) -> fastmcp.Client:
         headers = {}
         if enapter_auth_token is not None:
             headers["x-enapter-auth-token"] = enapter_auth_token
+        if enapter_auth_user is not None:
+            headers["x-enapter-auth-user"] = enapter_auth_user
         transport = fastmcp.client.StreamableHttpTransport(url, headers=headers)
         return fastmcp.Client(transport=transport)
 
