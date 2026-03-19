9f26a03521e5d4c34d5ae49718df904a0f7330fd
Roman Novatorov <roman.novatorov@gmail.com>
2026-03-19 14:51:42 +0100

    chore(setup): upgrade `enapter` to `0.17.0`

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def main() -> None:
         author="Roman Novatorov",
         author_email="rnovatorov@enapter.com",
         install_requires=[
-            "enapter==0.16.0",
+            "enapter==0.17.0",
             "fastmcp==2.14.*",
             "sentry-sdk==2.53.*",
             "httpx==0.*",
